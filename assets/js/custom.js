5467e73cbe1ca0ce84924b5e2291d7a3a319f68a
zoombree <linuodoc@gmail.com>
2022-04-13 05:34:05 +0400

    touchscroll-amend

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,28 +1,26 @@
-
 // const reloadtButton = document.querySelector("#reload");
 // function reload() {
 //     reload = location.reload();
 // }
 // reloadtButton.addEventListener("click", reload, false);
 
-
 // When the window has finished loading create our google map below
 // google.maps.event.addDomListener(window, 'load', init);
-$('.moreless-button').click(function() {
-  $('.moretext').slideToggle();
-  if ($('.moreless-button').text() == "Read more") {
-    $(this).text("Read less")
+$(".moreless-button").click(function () {
+  $(".moretext").slideToggle();
+  if ($(".moreless-button").text() == "Read more") {
+    $(this).text("Read less");
   } else {
-    $(this).text("Read more")
+    $(this).text("Read more");
   }
 });
 
 function init() {
-  console.log('map start')
+  console.log("map start");
   var myLatlng = new google.maps.LatLng(35.692056, 51.258393);
   var mapOptions = {
     zoom: 11,
-    center: myLatlng, 
+    center: myLatlng,
     styles: [
       { featureType: "administrative", elementType: "all", stylers: [{ saturation: "-100" }] },
       { featureType: "administrative.province", elementType: "all", stylers: [{ visibility: "off" }] },
@@ -40,9 +38,9 @@ function init() {
   var mapElement = document.getElementById("map");
   var map = myLatlng.Map(mapElement, mapOptions);
   var marker = myLatlng.Marker({
-      position: myLatlng.LatLng(40.6700, -73.9400),
-      map: map,
-      title: 'Snazzy!'
+    position: myLatlng.LatLng(40.67, -73.94),
+    map: map,
+    title: "Snazzy!",
   });
 }
 // ------------------Global Selectors;--------------------
@@ -60,13 +58,17 @@ const toTop = document.getElementById("scroll-to-top");
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
-  multiplier: 1,
-  lerp: 0.08,
-  smartphone: { 
-    smooth: true,
-    touchMultiplier: 2,
+  multiplier: 1.4,
+  lerp: 0.03,
+  touchMultiplier: 2,
+  smartphone: {
+    smooth: !0,
+    breakpoint: 767,
   },
-  tablet: { smooth: true },
+  tablet: {
+    smooth: !1,
+    breakpoint: 1024,
+  },
   smoothMobile: true,
   scrollFromAnywhere: false,
   getSpeed: true,
@@ -251,88 +253,85 @@ anchorLinks.forEach((anchorLink) => {
 // });
 
 ////////////////////////////////////////////////////////////////////
-// FORM 
+// FORM
 ////////////////////////////////////////////////////////////////////
 
 (function ($) {
   "use strict";
 
-
   /*==================================================================
   [ Validate after type ]*/
-  $('.validate-input .input100').each(function(){
-      $(this).on('blur', function(){
-          if(validate(this) == false){
-              showValidate(this);
-          }
-          else {
-              $(this).parent().addClass('true-validate');
-          }
-      })    
-  })
-
+  $(".validate-input .input100").each(function () {
+    $(this).on("blur", function () {
+      if (validate(this) == false) {
+        showValidate(this);
+      } else {
+        $(this).parent().addClass("true-validate");
+      }
+    });
+  });
 
   /*==================================================================
   [ Validate ]*/
-  var input = $('.validate-input .input100');
+  var input = $(".validate-input .input100");
 
-  $('.validate-form').on('submit',function(){
-      var check = true;
+  $(".validate-form").on("submit", function () {
+    var check = true;
 
-      for(var i=0; i<input.length; i++) {
-          if(validate(input[i]) == false){
-              showValidate(input[i]);
-              check=false;
-          }
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
       }
+    }
 
-      return check;
+    return check;
   });
 
-
-  $('.validate-form .input100').each(function(){
-      $(this).focus(function(){
-         hideValidate(this);
-         $(this).parent().removeClass('true-validate');
-      });
+  $(".validate-form .input100").each(function () {
+    $(this).focus(function () {
+      hideValidate(this);
+      $(this).parent().removeClass("true-validate");
+    });
   });
 
-   function validate (input) {
-      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-              return false;
-          }
+  function validate(input) {
+    if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
+      if (
+        $(input)
+          .val()
+          .trim()
+          .match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null
+      ) {
+        return false;
       }
-      else {
-          if($(input).val().trim() == ''){
-              return false;
-          }
+    } else {
+      if ($(input).val().trim() == "") {
+        return false;
       }
+    }
   }
 
   function showValidate(input) {
-      var thisAlert = $(input).parent();
+    var thisAlert = $(input).parent();
 
-      $(thisAlert).addClass('alert-validate');
+    $(thisAlert).addClass("alert-validate");
 
-      $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
-      $('.btn-hide-validate').each(function(){
-          $(this).on('click',function(){
-             hideValidate(this);
-          });
+    $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>');
+    $(".btn-hide-validate").each(function () {
+      $(this).on("click", function () {
+        hideValidate(this);
       });
+    });
   }
 
   function hideValidate(input) {
-      var thisAlert = $(input).parent();
-      $(thisAlert).removeClass('alert-validate');
-      $(thisAlert).find('.btn-hide-validate').remove();
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass("alert-validate");
+    $(thisAlert).find(".btn-hide-validate").remove();
   }
-  
-  
-
 })(jQuery);
 
 ////////////////////////////////////////////////////////////////////
-// FORM 
+// FORM
 ////////////////////////////////////////////////////////////////////
